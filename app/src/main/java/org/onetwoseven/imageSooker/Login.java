f9ecf83a97c718ffd65d7395465ed48302f75e6f
Alan Ainsworth <fruitbaticus@gmail.com>
2015-04-27 21:51:31 +0100

    Initial Commit

---
+++ app/src/main/java/org/onetwoseven/imageSooker/Login.java
@@ -0,0 +1,181 @@
+package org.onetwoseven.imageSooker;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.onetwoseven.imageSooker.uploader.AsyncUpload.uploadResults;
+import org.onetwoseven.imageSooker.uploaderCaption.CheckLoggedIn;
+
+import org.onetwoseven.imageSooker.R;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends Activity{
+	private SharedPreferences settings;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+//		CookieManager cookieManager = new CookieManager();
+//		CookieHandler.setDefault(cookieManager);
+		
+		
+		settings = getSharedPreferences("store",MODE_PRIVATE);
+//		
+//		Log.v(this.getClass().getName(),"Hmm " + settings.getString("username", ""));
+		//Log.v(this.getClass().getName(),Boolean.toString(settings.getString("username", null).equals(null)));
+		if(settings.contains("session")){
+			setResult(RESULT_OK);
+			finish();
+		}else{
+				setContentView(R.layout.login);
+				
+				Button btn = (Button) findViewById(R.id.login);
+	
+				btn.setOnClickListener(new View.OnClickListener() {
+				    @Override
+				    public void onClick(View v) {
+				        doLogin(v);
+				        
+				    }
+				});
+		}
+		
+		
+	}
+	
+	
+	public void doLogin(View view){
+		
+		//SharedPreferences settings = getSharedPreferences("store",MODE_PRIVATE);
+		
+		EditText usernameBox = (EditText)view.getRootView().findViewById(R.id.username);
+		EditText passwordBox = (EditText)view.getRootView().findViewById(R.id.password);
+		//CheckBox rememberBox = (CheckBox)view.getRootView().findViewById(R.id.remember);
+		
+		String  username = usernameBox.getText().toString();
+		String  password = passwordBox.getText().toString();
+		//Boolean  remember = rememberBox.isChecked();
+		
+		//String hash = sha1(username.toLowerCase() + password).toLowerCase();
+		
+		//String hostname = "http://10.0.1.99";
+		//String hostname = "http://10.0.1.154:8080";
+		//String path = "/Account/Login";
+
+		String[] LogInArgs = new String[2];
+		//LogInArgs[0] =  hostname + path;
+		LogInArgs[0] = username;
+		//LogInArgs[1] = password;
+		LogInArgs[1] = sha1(username.toLowerCase() + password).toLowerCase();
+		new LogIn().execute(LogInArgs);
+				
+		//username: username
+		//password2: lowercase(sha1(lowercase(login) + password))
+		
+		
+//	    SharedPreferences.Editor editor = settings.edit();
+//	    
+//	    editor.putString("username", username);
+//	    editor.putString("hash", hash);
+//	    
+//	    Log.v(this.getClass().getName(),hash);
+//	    
+//	    if(!remember){
+//	    	editor.putBoolean("consume", true);
+//	    }
+//	    editor.commit();
+	    
+		
+	}
+	
+	public String sha1(String input){
+		 
+		 try{
+			 MessageDigest md = MessageDigest.getInstance("SHA-1");
+		     ByteArrayInputStream fis = new ByteArrayInputStream(input.getBytes());
+		
+		     byte[] dataBytes = new byte[1024];
+		
+		     int nread = 0; 
+		     while ((nread = fis.read(dataBytes)) != -1) {
+		       md.update(dataBytes, 0, nread);
+		     };
+		     byte[] mdbytes = md.digest();
+		
+		     //convert the byte to hex format method 1
+		     StringBuffer sb = new StringBuffer();
+		     for (int i = 0; i < mdbytes.length; i++) {
+		       sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+		     }
+		     return sb.toString();
+		 }catch (NoSuchAlgorithmException e){
+			 return null;
+			 
+		 } catch (IOException e) {
+			return null;
+		}
+	 }
+	
+	
+	class LogIn extends AsyncTask<String,Void,Boolean>{
+
+		
+		@Override
+		protected Boolean doInBackground(String... args) {
+			
+			//String uri = args[0];
+			String username = args[0];
+			String password = args[1];
+
+			if(new Network(getBaseContext()).logIn(username, password)){
+				return true;
+			}else{
+				return false;
+			}
+			
+		}
+		
+		protected void onPostExecute(Boolean status){
+			//SharedPreferences settings = getSharedPreferences("store",MODE_PRIVATE);
+			SharedPreferences.Editor edit1 = settings.edit();
+						
+			if(status){
+				edit1.putBoolean("LoggedIn", true);
+				edit1.commit();
+				setResult(RESULT_OK);
+		        finish();
+					
+			}else{
+				edit1.clear();
+				edit1.commit();
+				
+			}
+			
+			
+		}
+
+		
+	}
+
+	
+}
---
+++ app/src/main/java/org/onetwoseven/imageSooker/uploader.java
@@ -0,0 +1,295 @@
+package org.onetwoseven.imageSooker;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.CookiePolicy;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+
+import org.onetwoseven.imageSooker.R;
+
+//import org.apache.commons.codec_1_4.binary.Base64;
+
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+public class uploader extends Activity {
+	public final static String CAPTION = "org.onetwoseven.imageSooker.CAPTION";
+	Intent intent;
+	Bundle extras;
+	String action;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		CookieManager cookieManager = new CookieManager();
+		CookieHandler.setDefault(cookieManager);
+		
+		
+		// setContentView(R.layout.main);
+
+		intent = getIntent();
+		extras = intent.getExtras();
+		action = intent.getAction();
+//hmm
+		Intent login = new Intent(this, Login.class);
+		
+		startActivityForResult(login, 1);
+		
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		if(resultCode == RESULT_OK){
+			boolean lastPass = false;
+			if(Intent.ACTION_SEND.equals(action))
+			{
+				if(extras.containsKey(Intent.EXTRA_STREAM))
+				{
+					try
+					{
+						Uri uri = (Uri) extras.getParcelable(Intent.EXTRA_STREAM);
+						if(extras.containsKey(CAPTION))
+						{
+							if(!extras.getString(CAPTION).isEmpty()){
+								Log.v(this.getClass().getName(),"caption:" + extras.getString(CAPTION) + "#");
+								new AsyncUpload().execute(lastPass,uri,extras.getString(CAPTION));
+							}else{
+								new AsyncUpload().execute(lastPass,uri);
+
+							}
+						}else{
+							new AsyncUpload().execute(lastPass,uri);
+						}
+						finish();
+						return;
+
+					}
+					catch (Exception e)
+					{
+						Log.e(this.getClass().getName(),e.toString());
+
+					}
+				}
+			}
+			if(Intent.ACTION_SEND_MULTIPLE.equals(action)){
+				if(extras.containsKey(Intent.EXTRA_STREAM)){
+					try{
+						ArrayList<Parcelable> list = 
+								intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+						int pass = 0;
+						int len = list.size();
+
+						for (Parcelable p : list) {
+							lastPass = pass + 1 == len;
+
+
+							pass = pass + 1;
+							Uri uri = (Uri) p; 
+							if(extras.containsKey(CAPTION)){
+								if(!extras.getString(CAPTION).isEmpty()){
+									String cap = extras.getString(CAPTION);
+									cap = cap + "_" + Integer.toString(pass);
+									new AsyncUpload().execute(lastPass,uri,cap);
+								}else{
+									new AsyncUpload().execute(lastPass,uri);
+								}
+
+							}else{
+								new AsyncUpload().execute(lastPass,uri);
+							}
+						}
+						finish();
+					}catch(Exception e){
+						Log.e(this.getClass().getName(),e.toString());
+					}
+				}
+			}
+
+		}
+
+
+		
+
+
+	}
+	
+	
+
+	public class AsyncUpload extends AsyncTask<Object, Integer, String[]> {
+		private final ProgressDialog dialog = new ProgressDialog(uploader.this);
+
+		NotificationCompat.Builder mBuilder;
+		NotificationManager mNotifyManager;
+
+		@Override
+		protected void onPreExecute() {
+			mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+			mBuilder = new NotificationCompat.Builder(uploader.this)
+			.setSmallIcon(R.drawable.ic_stat_upload)
+			.setContentTitle("Image Uploading...")
+			.setContentText("Starting...");
+
+			mNotifyManager.notify(0, mBuilder.build());
+			
+		}
+
+		@Override
+		protected String[] doInBackground(Object... data) {
+			boolean lastPass = (Boolean) data[0];
+			Uri myUri = (Uri) data[1];
+
+			String result = "";
+			String imageUrl = "";
+
+			try {
+
+				ContentResolver cr = getContentResolver();
+				InputStream is = cr.openInputStream(myUri);
+
+				String mime = cr.getType(myUri);
+				String name = "";
+				int size = 0;
+				String[] proj = { MediaStore.MediaColumns.DATA,
+						MediaStore.MediaColumns.SIZE };
+				Cursor mc = cr.query(myUri, proj, null, null, null);
+				if (mc != null) {
+					try {
+						if (mc.moveToFirst()) {
+							name = mc.getString(0);
+							size = mc.getInt(1);
+						}
+
+					} finally {
+						mc.close();
+					}
+				}
+				String myCaption = null;
+				if (data.length > 2) {
+					myCaption = (String) data[2];
+				}
+
+				Log.v(this.getClass().getName(),
+						"File size: " + Integer.toString(size) + " Mime: "
+								+ mime);
+
+				mBuilder.setContentTitle("Uploading " + (myCaption != null ? myCaption : name));
+				
+				SharedPreferences settings = getSharedPreferences("store", 0);
+				
+				Log.v(this.getClass().getName(),"Trying caption: " + myCaption);
+				// Upload file
+				
+				uploadResults results = new Network(getBaseContext()).uploadImage(is, name, size, mime,
+						myCaption,this);
+
+				int statusCode = results.statusCode;
+
+				if (statusCode == 200) {
+					result = "Something went sideways :(";
+//					SharedPreferences.Editor edit1 = settings.edit();
+//					edit1.clear();
+//					edit1.commit();
+
+				} else if (statusCode == 302) {
+					result = "Image Uploaded to: " + results.imgLoc;
+//					SharedPreferences.Editor edit1 = settings.edit();
+//					edit1.clear();
+//					edit1.commit();
+					imageUrl = results.imgLoc;
+				} else {
+					result = "Got " + Integer.toString(statusCode) + " code";
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				Log.v(this.getClass().getName(), e.getStackTrace().toString());
+				result = "Something went wrong :(";
+			}
+
+			return new String[] { result, imageUrl };
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+			try {
+				//Log.v(this.getClass().getName(), Integer.toString(values[0]));
+				mBuilder.setProgress(100, values[0], false);
+				mBuilder.setContentText(Integer.toString(values[0]) + "%");
+				mNotifyManager.notify(0, mBuilder.build());
+			} catch (Exception e) {
+
+				Log.v(this.getClass().getName(),
+						"Values is empty? " + e.toString());
+			}
+			// Update percentage
+		}
+
+		@Override
+		protected void onPostExecute(String[] result) {
+			super.onPostExecute(result);
+			mBuilder.setContentText(result[0]);
+			mBuilder.setProgress(0, 0, false);
+			if (result[1] != "") {
+				mBuilder.setContentTitle("Done");
+
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(result[1]));
+
+				PendingIntent pI = PendingIntent.getActivity(uploader.this, 0,
+						i, PendingIntent.FLAG_ONE_SHOT);
+				mBuilder.setContentIntent(pI);
+			} else {
+				mBuilder.setContentTitle("");
+			}
+			mBuilder.setAutoCancel(true);
+			mNotifyManager.notify(0, mBuilder.build());
+			// Toast.makeText(uploader.this,
+			// result[0],Toast.LENGTH_LONG).show();
+		}
+		
+		public void myPublish(int progress){
+			publishProgress(progress);
+		}
+
+		class uploadResults {
+			public final int statusCode;
+			public final String imgLoc;
+
+			public uploadResults(int a, String b) {
+				this.statusCode = a;
+				this.imgLoc = b;
+			}
+
+		}
+
+		
+
+	}
+
+}
