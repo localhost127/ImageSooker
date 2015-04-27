6226137cc16cb73537b304a5fcba4d88e316b9d1
Alan Ainsworth <fruitbaticus@gmail.com>
2015-04-27 22:20:28 +0100

    Commented two newlines in setupConnection. Reformatted source. Removed unused imports.

--- app/src/main/java/org/onetwoseven/imageSooker/Network.java
+++ app/src/main/java/org/onetwoseven/imageSooker/Network.java
@@ -11,226 +11,216 @@ import org.onetwoseven.imageSooker.uploader.AsyncUpload.uploadResults;
 
 import android.content.SharedPreferences;
 import android.util.Log;
-import android.webkit.CookieManager;
+
 import android.content.Context;
 
 
-
 public class Network {
-	public String IP = "http://img.127001.org";
-	private String Boundary = "----xxxRRRFFFfffxxx----";
-	private String Newline = "\r\n";
-	private int Buffsize = 10240;
-	
-	private HttpURLConnection conn;
-	private Context context;
-	private SharedPreferences settings;
-	
-	public Network(Context ctx){
-		context = ctx;
-		settings = context.getSharedPreferences("store",Context.MODE_PRIVATE);
-	}
-	
-	public boolean checkLoggedIn(){
-		DataOutputStream foo = setupConnection(IP + "/loggedin.php");
-		try{
-			foo.flush();
-			int responseCode = conn.getResponseCode();
-			
-			Log.v(this.getClass().getName(), "Response Code:" + Integer.toString(responseCode));
-			if(responseCode == 200){
-				return true;
-			}
-		}catch (Exception e){
-			Log.v(this.getClass().getName(), e.toString());
-		}finally{
-			conn.disconnect();
-		}
-		return false;
-		
-	}
-	
-	
-	
-	public boolean logIn(String username, String password){
-		
-		
-		SharedPreferences.Editor edit1 = settings.edit();
-		
-		DataOutputStream foo = setupConnection(IP + "/loggedin.php");
-		try{
-						
-			// Oh god, password
-			foo.writeBytes("--" + Boundary + Newline);
-			foo.writeBytes("Content-Disposition: form-data; name=\"username\"" + Newline + Newline
-					+ username + Newline);
-			
-			foo.writeBytes("--" + Boundary + Newline);
-			foo.writeBytes("Content-Disposition: form-data; name=\"remember\"" + Newline + Newline
-					+ "true" + Newline);
-			
-			
-			
-			foo.writeBytes("--" + Boundary + Newline);
-			foo.writeBytes("Content-Disposition: form-data; name=\"password2\"" + Newline + Newline
-					+ password + Newline);
-			
-			
-			foo.writeBytes("--" + Boundary + "--" + Newline);
-			foo.flush();
-			int responseCode = conn.getResponseCode();
-			Log.v(this.getClass().getName(), "Response Code:" + Integer.toString(responseCode));
-			if(responseCode == 200){
-				List<String> cookieList = conn.getHeaderFields().get("Set-Cookie");
+    public String IP = "http://img.127001.org";
+    private String Boundary = "----xxxRRRFFFfffxxx----";
+    private String Newline = "\r\n";
+    private int Buffsize = 10240;
+
+    private HttpURLConnection conn;
+    private Context context;
+    private SharedPreferences settings;
+
+    public Network(Context ctx) {
+        context = ctx;
+        settings = context.getSharedPreferences("store", Context.MODE_PRIVATE);
+    }
+
+    public boolean checkLoggedIn() {
+        DataOutputStream foo = setupConnection(IP + "/loggedin.php");
+        try {
+            foo.flush();
+            int responseCode = conn.getResponseCode();
+
+            Log.v(this.getClass().getName(), "Response Code:" + Integer.toString(responseCode));
+            if (responseCode == 200) {
+                return true;
+            }
+        } catch (Exception e) {
+            Log.v(this.getClass().getName(), e.toString());
+        } finally {
+            conn.disconnect();
+        }
+        return false;
+
+    }
+
+
+    public boolean logIn(String username, String password) {
+
+
+        SharedPreferences.Editor edit1 = settings.edit();
+
+        DataOutputStream foo = setupConnection(IP + "/loggedin.php");
+        try {
+
+            // Oh god, password
+            foo.writeBytes("--" + Boundary + Newline);
+            foo.writeBytes("Content-Disposition: form-data; name=\"username\"" + Newline + Newline
+                    + username + Newline);
+
+            foo.writeBytes("--" + Boundary + Newline);
+            foo.writeBytes("Content-Disposition: form-data; name=\"remember\"" + Newline + Newline
+                    + "true" + Newline);
+
+
+            foo.writeBytes("--" + Boundary + Newline);
+            foo.writeBytes("Content-Disposition: form-data; name=\"password2\"" + Newline + Newline
+                    + password + Newline);
+
+
+            foo.writeBytes("--" + Boundary + "--" + Newline);
+            foo.flush();
+            int responseCode = conn.getResponseCode();
+            Log.v(this.getClass().getName(), "Response Code:" + Integer.toString(responseCode));
+            if (responseCode == 200) {
+                List<String> cookieList = conn.getHeaderFields().get("Set-Cookie");
 //				for(List<String> headers : conn.getHeaderFields().values()){
 //					
 //					for(String header: headers){
 //						Log.v(this.getClass().getName(), "Header:" + header);
 //					}
 //				}
-				if(cookieList != null){
-					for(String cookieTemp : cookieList){
-						Log.v(this.getClass().getName(), "Cookie:" + cookieTemp);
-						if(cookieTemp.startsWith("imgsooker=")){
-							edit1.putString("session", cookieTemp.substring(0, cookieTemp.indexOf(";")));
-							Log.v(this.getClass().getName(), "Saved cookie!" + cookieTemp);
-							edit1.commit();
-							
-							
-						}
-					}
-					
-				}
-				return true;
-			}
-		}catch (Exception e){
-			Log.v(this.getClass().getName(), e.toString());
-		}finally{
-			conn.disconnect();
-		}
-		return false;
-		
-	}
-	
-	
-	public uploadResults uploadImage(InputStream is, String name,
-			int size, String mime, String caption, AsyncUpload uploadInstance) {
-		//String hostname = "http://10.0.1.99";
-		
-		String path = "/index.php";
+                if (cookieList != null) {
+                    for (String cookieTemp : cookieList) {
+                        Log.v(this.getClass().getName(), "Cookie:" + cookieTemp);
+                        if (cookieTemp.startsWith("imgsooker=")) {
+                            edit1.putString("session", cookieTemp.substring(0, cookieTemp.indexOf(";")));
+                            Log.v(this.getClass().getName(), "Saved cookie!" + cookieTemp);
+                            edit1.commit();
 
-		DataOutputStream foo = setupConnection(IP + path);
-		
-		try{
-			foo.writeBytes("--" + Boundary + Newline);
-			
-			
-			if (caption != null) {
 
-				foo.writeBytes("Content-Disposition: form-data; name=\"caption\"" + Newline + Newline
-						+ caption + Newline);
-				foo.writeBytes("--" + Boundary + Newline);
-			}
+                        }
+                    }
 
-			foo.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\"; filename=\""
-					+ name + "\"" + Newline);
-			foo.writeBytes("Content-Type: " + mime + Newline + Newline);
-		
+                }
+                return true;
+            }
+        } catch (Exception e) {
+            Log.v(this.getClass().getName(), e.toString());
+        } finally {
+            conn.disconnect();
+        }
+        return false;
 
-			// Send data
-			int bytesAvailable = is.available();
+    }
 
-			bytesAvailable = Math.min(bytesAvailable, Buffsize);
 
-			byte[] buffer = new byte[bytesAvailable];
+    public uploadResults uploadImage(InputStream is, String name,
+                                     int size, String mime, String caption, AsyncUpload uploadInstance) {
+        //String hostname = "http://10.0.1.99";
 
-			int bytesRead = is.read(buffer, 0, Buffsize);
-			int noChunks = 1;
+        String path = "/index.php";
 
-			int progress = 0;
+        DataOutputStream foo = setupConnection(IP + path);
 
-			while (bytesRead > 0) {
+        try {
+            foo.writeBytes("--" + Boundary + Newline);
 
-				foo.write(buffer, 0, bytesAvailable);
-				bytesAvailable = is.available();
-				bytesAvailable = Math.min(bytesAvailable, Buffsize);
 
-				bytesRead = is.read(buffer, 0, bytesAvailable);
-				//Log.v(this.getClass().getName(),
+            if (caption != null) {
+
+                foo.writeBytes("Content-Disposition: form-data; name=\"caption\"" + Newline + Newline
+                        + caption + Newline);
+                foo.writeBytes("--" + Boundary + Newline);
+            }
+
+            foo.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\"; filename=\""
+                    + name + "\"" + Newline);
+            foo.writeBytes("Content-Type: " + mime + Newline + Newline);
+
+
+            // Send data
+            int bytesAvailable = is.available();
+
+            bytesAvailable = Math.min(bytesAvailable, Buffsize);
+
+            byte[] buffer = new byte[bytesAvailable];
+
+            int bytesRead = is.read(buffer, 0, Buffsize);
+            int noChunks = 1;
+
+            int progress = 0;
+
+            while (bytesRead > 0) {
+
+                foo.write(buffer, 0, bytesAvailable);
+                bytesAvailable = is.available();
+                bytesAvailable = Math.min(bytesAvailable, Buffsize);
+
+                bytesRead = is.read(buffer, 0, bytesAvailable);
+                //Log.v(this.getClass().getName(),
 //						"Progress: " + Integer.toString(bytesAvailable)
 //						+ " " + Integer.toString(Buffsize) + " "
 //						+ Integer.toString(bytesRead));
-				int bar = (int) (((float) (noChunks * Buffsize) / size) * 100);
-				if (bar != progress) {
-					progress = bar;
-					uploadInstance.myPublish(progress);
-				}
+                int bar = (int) (((float) (noChunks * Buffsize) / size) * 100);
+                if (bar != progress) {
+                    progress = bar;
+                    uploadInstance.myPublish(progress);
+                }
 
-				noChunks = noChunks + 1;
-				// dos.flush();
+                noChunks = noChunks + 1;
+                // dos.flush();
 
-			}
-			foo.writeBytes(Newline);
-			foo.writeBytes("--" + Boundary + "--" + Newline);
-			foo.flush();
-			int statusCode = conn.getResponseCode();
+            }
+            foo.writeBytes(Newline);
+            foo.writeBytes("--" + Boundary + "--" + Newline);
+            foo.flush();
+            int statusCode = conn.getResponseCode();
 
-			return uploadInstance.new uploadResults(statusCode,
-					conn.getHeaderField("Location"));
-			
-		}catch(Exception e){
-			return uploadInstance.new uploadResults(0,"");
-		}
-			
-		
-	}
+            return uploadInstance.new uploadResults(statusCode,
+                    conn.getHeaderField("Location"));
 
-	
-	
-	private DataOutputStream setupConnection(String uri){
-		
-		
-				
-		DataOutputStream dos;
-		
-		try{
-			URL url = new URL(uri);
-			conn = (HttpURLConnection) url
-					.openConnection();
+        } catch (Exception e) {
+            return uploadInstance.new uploadResults(0, "");
+        }
+
+
+    }
+
+
+    private DataOutputStream setupConnection(String uri) {
+
+
+        DataOutputStream dos;
+
+        try {
+            URL url = new URL(uri);
+            conn = (HttpURLConnection) url
+                    .openConnection();
 
 //			CookieManager cookieManager = CookieManager.getInstance();
 //			String cookie = cookieManager.getCookie(conn.getURL().toString());
-			String cookie = settings.getString("session", null);
-			if (cookie != null){
-				Log.v(this.getClass().getName(), "Sending cookie: " + cookie);
-				conn.setRequestProperty("Cookie", cookie);
-			}
-			
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Connection", "Keep-Alive");
-			conn.setRequestProperty("Content-Type",
-					"multipart/form-data; boundary=" + Boundary);
-			conn.setChunkedStreamingMode(Buffsize);
-			conn.setInstanceFollowRedirects(false);
+            String cookie = settings.getString("session", null);
+            if (cookie != null) {
+                Log.v(this.getClass().getName(), "Sending cookie: " + cookie);
+                conn.setRequestProperty("Cookie", cookie);
+            }
 
-			
-			dos = new DataOutputStream(
-					conn.getOutputStream());
-			dos.writeBytes(Newline);
-			dos.writeBytes(Newline);
-			
-			
-			
-		
-		}catch (Exception e){
-			Log.v(this.getClass().getName(), e.toString());
-			dos = null;
-		}
-		return dos;
-	}
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type",
+                    "multipart/form-data; boundary=" + Boundary);
+            conn.setChunkedStreamingMode(Buffsize);
+            conn.setInstanceFollowRedirects(false);
 
-	
-	
-	
+
+            dos = new DataOutputStream(
+                    conn.getOutputStream());
+            //dos.writeBytes(Newline);
+            //dos.writeBytes(Newline);
+
+
+        } catch (Exception e) {
+            Log.v(this.getClass().getName(), e.toString());
+            dos = null;
+        }
+        return dos;
+    }
+
 
 }
--- app/src/main/java/org/onetwoseven/imageSooker/uploaderCaption.java
+++ app/src/main/java/org/onetwoseven/imageSooker/uploaderCaption.java
@@ -1,28 +1,17 @@
 package org.onetwoseven.imageSooker;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+
 import java.net.CookieHandler;
 import java.net.CookieManager;
-import java.net.CookiePolicy;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
-import org.onetwoseven.imageSooker.Network;
 
-import org.onetwoseven.imageSooker.R;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.InputType;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -30,151 +19,145 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class uploaderCaption extends Activity {
-	public final static String CAPTION = "org.onetwoseven.imageSooker.CAPTION";
-	Intent j;
-	Intent login;
-	
-	
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		
-		CookieManager cookieManager = new CookieManager();
-		CookieHandler.setDefault(cookieManager);
-		
-		j = getIntent();
-		
+    public final static String CAPTION = "org.onetwoseven.imageSooker.CAPTION";
+    Intent j;
+    Intent login;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CookieManager cookieManager = new CookieManager();
+        CookieHandler.setDefault(cookieManager);
+
+        j = getIntent();
+
 		/*login = new Intent(this,Login.class);
 		
 		startActivityForResult(login, 1);*/
-		
-		
-		//String hostname = "http://10.0.1.99";
-		//String hostname = "http://10.0.1.154:8080";
-		//String path = "/Forests";
-		//new CheckLoggedIn().execute(hostname + path);
-		new CheckLoggedIn().execute();
-		
-		setContentView(R.layout.caption);
-		
-		Button okBtn = (Button) findViewById(R.id.okBtn);
-		Button notYouBtn = (Button) findViewById(R.id.notYouBtn);
-		notYouBtn.setEnabled(false);
-		Button cancelBtn = (Button) findViewById(R.id.cancelBtn);
-		
-		okBtn.setOnClickListener(new View.OnClickListener() {
-		    @Override
-		    public void onClick(View v) {
-		        doUpload(v);
-		    }
-		});
-		notYouBtn.setOnClickListener(new View.OnClickListener() {
-		    @Override
-		    public void onClick(View v) {
-		        notYou(v);
-		        
-		    }
-		});
-		
-		cancelBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				cancel(v);
-				
-			}
-		});
-		
-	}	
-	
-	public void doUpload(View view){
-		 
-		
-	 
-		final Intent i = new Intent(this,uploader.class);
-				
-		EditText userInput = (EditText)findViewById(R.id.captionEdit);
-		
-			
-		String caption = userInput.getText().toString();
-	  
-		i.putExtra(CAPTION, caption);
-		i.putExtras(j.getExtras());
-		i.setAction(j.getAction());
-		startActivity(i);
-		setResult(RESULT_OK);
-		finish();
-	
-		
-		
-				
-		
-	}
-	public void notYou(View view){
-		SharedPreferences settings = getSharedPreferences("store",MODE_PRIVATE);
-		SharedPreferences.Editor edit1 = settings.edit();
-		edit1.clear();
-		edit1.commit();
-		TextView txtView = (TextView) findViewById(R.id.userReminder);
-		Button notYouBtn = (Button) findViewById(R.id.notYouBtn);
-		txtView.setText("Not Logged In");
-		notYouBtn.setEnabled(false);
-		//Intent login = new Intent(this, Login.class);
-		//startActivityForResult(login, 1);
-		
-	}
-	
-	public void cancel(View view){
-		setResult(RESULT_CANCELED);
-		finish();
-		
-	}
-	
-	class CheckLoggedIn extends AsyncTask<String,Void,Boolean>{
 
-		
-		@Override
-		protected Boolean doInBackground(String... uri) {
-			Log.v(this.getClass().getName(), "Checking for logged in status");
-				if (new Network(getBaseContext()).checkLoggedIn()){
-					return true;
-				}else{
-					return false;
-				}
-				
-			
-		}
-		
-		protected void onPostExecute(Boolean status){
-			SharedPreferences settings = getSharedPreferences("store",MODE_PRIVATE);
-			SharedPreferences.Editor edit1 = settings.edit();
-			
-			TextView txtView = (TextView) findViewById(R.id.userReminder);
-			Button notYouBtn = (Button) findViewById(R.id.notYouBtn);
-			
-			
-			if(status){
-				notYouBtn.setEnabled(true);
-				edit1.putBoolean("LoggedIn", true);
-				
-				
-				txtView.setText("Logged In");
-				
-			}else{
-				edit1.clear();
-				txtView.setText("Not Logged In");
-			}
-			edit1.commit();
-			
-		}
 
-		
-	}
+        //String hostname = "http://10.0.1.99";
+        //String hostname = "http://10.0.1.154:8080";
+        //String path = "/Forests";
+        //new CheckLoggedIn().execute(hostname + path);
+        new CheckLoggedIn().execute();
 
-	
-	
+        setContentView(R.layout.caption);
+
+        Button okBtn = (Button) findViewById(R.id.okBtn);
+        Button notYouBtn = (Button) findViewById(R.id.notYouBtn);
+        notYouBtn.setEnabled(false);
+        Button cancelBtn = (Button) findViewById(R.id.cancelBtn);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doUpload(v);
+            }
+        });
+        notYouBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notYou(v);
+
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                cancel(v);
+
+            }
+        });
+
+    }
+
+    public void doUpload(View view) {
+
+
+        final Intent i = new Intent(this, uploader.class);
+
+        EditText userInput = (EditText) findViewById(R.id.captionEdit);
+
+
+        String caption = userInput.getText().toString();
+
+        i.putExtra(CAPTION, caption);
+        i.putExtras(j.getExtras());
+        i.setAction(j.getAction());
+        startActivity(i);
+        setResult(RESULT_OK);
+        finish();
+
+
+    }
+
+    public void notYou(View view) {
+        SharedPreferences settings = getSharedPreferences("store", MODE_PRIVATE);
+        SharedPreferences.Editor edit1 = settings.edit();
+        edit1.clear();
+        edit1.commit();
+        TextView txtView = (TextView) findViewById(R.id.userReminder);
+        Button notYouBtn = (Button) findViewById(R.id.notYouBtn);
+        txtView.setText("Not Logged In");
+        notYouBtn.setEnabled(false);
+        //Intent login = new Intent(this, Login.class);
+        //startActivityForResult(login, 1);
+
+    }
+
+    public void cancel(View view) {
+        setResult(RESULT_CANCELED);
+        finish();
+
+    }
+
+    class CheckLoggedIn extends AsyncTask<String, Void, Boolean> {
+
+
+        @Override
+        protected Boolean doInBackground(String... uri) {
+            Log.v(this.getClass().getName(), "Checking for logged in status");
+            if (new Network(getBaseContext()).checkLoggedIn()) {
+                return true;
+            } else {
+                return false;
+            }
+
+
+        }
+
+        protected void onPostExecute(Boolean status) {
+            SharedPreferences settings = getSharedPreferences("store", MODE_PRIVATE);
+            SharedPreferences.Editor edit1 = settings.edit();
+
+            TextView txtView = (TextView) findViewById(R.id.userReminder);
+            Button notYouBtn = (Button) findViewById(R.id.notYouBtn);
+
+
+            if (status) {
+                notYouBtn.setEnabled(true);
+                edit1.putBoolean("LoggedIn", true);
+
+
+                txtView.setText("Logged In");
+
+            } else {
+                edit1.clear();
+                txtView.setText("Not Logged In");
+            }
+            edit1.commit();
+
+        }
+
+
+    }
+
+
 }
 
 
